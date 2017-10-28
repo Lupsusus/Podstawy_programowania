bfc7c72617e50dd35e74da9381c1bc701b12cf4b
aleksander.somla <aleksander.somla@gmail.com>
2017-10-28 14:40:48 +0200

    zad2

---
+++ zad1_cw5/zad2_cw5/zad2cw5.cpp
@@ -0,0 +1,60 @@
+//2. Dana jest tablica dwuwymiarowej o wymiarach NxN.Napisz program,
+//który obliczy średnią spośród elementów :
+//•	całej tablicy,
+//•	głównej przekątnej,
+//•	każdego wiersza osobno,
+//•	każdej kolumny osobno.
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	float csr = 0, psr = 0, wsr = 0, ksr = 0;
+	int el_licz = 0;
+	const int n = 4;
+	float  T[n][n];
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cout << "Podaj [" << i << "][" << j << "]" << endl;
+			cin >> T[i][j];
+			csr = csr + T[i][j];
+			el_licz++;
+		}
+	}
+	csr = csr / el_licz;
+	el_licz = 0;
+	for (int i = 0; i < n; i++) {
+		psr = psr + T[i][el_licz];
+		el_licz++;
+	}
+	psr = psr / el_licz;
+	el_licz = 0;
+	cout << "Srednia calej tablicy to " << csr << endl;
+	cout << "Srednia glownej przekatnej to " << psr << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			wsr = wsr + T[i][j];
+			el_licz++;
+		}
+		wsr = wsr / el_licz;
+		cout << "Srednia wiersza " << i << " to " << wsr << endl;
+		wsr = 0;
+		el_licz = 0;
+	}
+	for (int j = 0; j < n; j++) {
+		for (int i = 0; i < n; i++) {
+			ksr = ksr + T[i][j];
+			el_licz++;
+		}
+		ksr = ksr / el_licz;
+		cout << "Srednia kolumny " << j << " to " << ksr << endl;
+		ksr = 0;
+		el_licz = 0;
+	}
+	system("pause");
+	return 0;
+}
+
