dcc607c9bcc6dd1915efe6ac44bf39c346982011
aleksander.somla <aleksander.somla@gmail.com>
2017-10-28 15:10:32 +0200

    komentarz

--- cw5/zad1_cw5/zad1cw5.cpp
+++ cw5/zad1_cw5/zad1cw5.cpp
@@ -22,21 +22,21 @@ int main()
 	T = new int[n];
 	for (int i = 0; i < n; i++) {
 		cout << "Podaj " << i + 1 << " wartosc elementu: " << endl;
-		cin >> T[i];
+		cin >> T[i]; // wpisuje wartosci do tablicy 
 		if (i == 0) {
-			min = T[i];
+			min = T[i]; // dla pierwszego wpisu ustawiam wartosc minimalna
 		}
-		else if (T[i] < min) {
-			min = T[i];
+		else if (T[i] < min) { // dla kazdego kolejnego sprawdzam czy jest mniejszy
+			min = T[i]; // i ustawiam go jako najmniejszy
 		}
-		sr = sr + T[i];
+		sr = sr + T[i]; // przy okazji licze sume elementow
 	}
-	for (int i = 0; i < n; i++) {
-		for (j = k; j < n - (i + 1); j++)
+	for (int i = 0; i < n; i++) { // to jest sortowanie babelkowe
+		for (j = k; j < n - (i + 1); j++) // kazdy element jest sprawdzany czy jest mniejszy od najmnijeszego i przesuwany na nizsze miejsce w tabeli
 		{
 			if (T[j + 1] < T[j])
 			{
-				pom = T[j + 1];
+				pom = T[j + 1]; // zmienna pom przechowuje element tablicy w momencie wpisania na jego miejsce mniejszej wartosci
 				T[j + 1] = T[j];
 				T[j] = pom;
 			}
@@ -52,13 +52,13 @@ int main()
 		}
 		k++;
 	}
-	sr = sr / n;
-	if (n % 2 == 0) {
-		med = T[(n - 1) / 2] + T[n / 2];
+	sr = sr / n; // tutaj jest liczona srednia
+	if (n % 2 == 0) { //jesli ilosc elementow jest parzysta to wedlug wzoru 
+		med = T[(n - 1) / 2] + T[n / 2]; // licze mediane ze wzoru
 		med = med / 2;
 	}
 	else {
-		med = T[n / 2];
+		med = T[n / 2]; // jesli ilosc elementow jest nie parzysta wyznaczam element po srodku tabeli 
 	}
 	cout << "Element minimalny to: " << min << endl;
 	cout << "Srednia to " << sr << endl;
