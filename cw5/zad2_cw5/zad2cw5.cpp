b161d4497ef45347986261468b7b9e71dde4edb1
aleksander.somla <aleksander.somla@gmail.com>
2017-10-28 15:03:18 +0200

    komentarz

--- cw5/zad2_cw5/zad2cw5.cpp
+++ cw5/zad2_cw5/zad2cw5.cpp
@@ -19,38 +19,38 @@ int main()
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cout << "Podaj [" << i << "][" << j << "]" << endl;
-			cin >> T[i][j];
-			csr = csr + T[i][j];
-			el_licz++;
+			cin >> T[i][j]; //wpisuje dane do tablicy
+			csr = csr + T[i][j]; // obliczam sume elementow
+			el_licz++; // obliczam ilosc elementow
 		}
 	}
-	csr = csr / el_licz;
-	el_licz = 0;
+	csr = csr / el_licz; // licze srednia
+	el_licz = 0; // zeruje licznik
 	for (int i = 0; i < n; i++) {
-		psr = psr + T[i][el_licz];
-		el_licz++;
+		psr = psr + T[i][el_licz]; //sumuje elementy przekatnej
+		el_licz++; // i ilosc elementow
 	}
-	psr = psr / el_licz;
+	psr = psr / el_licz; // licze srednia przekatnej 
 	el_licz = 0;
 	cout << "Srednia calej tablicy to " << csr << endl;
 	cout << "Srednia glownej przekatnej to " << psr << endl;
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) { // dla kolejnego wiersza
 		for (int j = 0; j < n; j++) {
-			wsr = wsr + T[i][j];
-			el_licz++;
+			wsr = wsr + T[i][j]; // sumuje wszystkie jego elementy
+			el_licz++; // licze ich ilosc
 		}
-		wsr = wsr / el_licz;
-		cout << "Srednia wiersza " << i << " to " << wsr << endl;
+		wsr = wsr / el_licz; //licze srednia
+		cout << "Srednia wiersza " << i << " to " << wsr << endl; // wypisuje i lece od nowa dla kolejnego wiersza
 		wsr = 0;
 		el_licz = 0;
 	}
-	for (int j = 0; j < n; j++) {
+	for (int j = 0; j < n; j++) { //dla kolejnej kolumny 
 		for (int i = 0; i < n; i++) {
-			ksr = ksr + T[i][j];
-			el_licz++;
+			ksr = ksr + T[i][j]; // sumuje jej elementy
+			el_licz++; // ilosc
 		}
-		ksr = ksr / el_licz;
-		cout << "Srednia kolumny " << j << " to " << ksr << endl;
+		ksr = ksr / el_licz; // srednia
+		cout << "Srednia kolumny " << j << " to " << ksr << endl; // wypisuje i od nowa
 		ksr = 0;
 		el_licz = 0;
 	}
