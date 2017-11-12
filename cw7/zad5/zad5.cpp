bac5dde7d0f6aff94184b35ce6a8a88b281e4138
aleksander.somla <aleksander.somla@gmail.com>
2017-11-12 17:58:09 +0100

    cw7

--- cw7/zad5/zad5.cpp
+++ cw7/zad5/zad5.cpp
@@ -4,38 +4,6 @@
 //schematu Hornera.
 
 #include "stdafx.h"
-//#include <iostream>
-//using namespace std;
-//
-//int main()
-//{
-//	int *T;         // tworzymy wskaźnik
-//	int i, n, x, temp;
-//	x = 0;
-//	cout << "Podaj najwieksza potege wielomianu:";
-//	cin >> n;        // odczytujemy ilość komórek
-//	T = new int[n];  // tworzymy tablicę dynamiczną o n komórkach
-//	for (i = 0; i <= n; i++)
-//	{
-//		cout << "Podaj wspolczynnik potegi " << i << ": ";
-//		cin >> T[i];   // wczytujemy kolejne komórki
-//	}
-//	cout << "Podaj x dla ktorego ma zostac wyznaczona wartosc wielomianu: ";
-//	cin >> x;
-//	temp = T[n];
-//	cout << "Stos: " << endl;
-//	cout << T[n] << endl;
-//	for (i = n-1; i >= 0; i--)
-//	{
-//		cout << T[i] << endl; //wypisuje kolejne elementy stosu
-//		temp = x*temp + T[i]; //wykonuje kolejne dzialania ze schematu hornera
-//	}
-//	cout << "Wynik: " << endl;
-//	cout << temp << endl;
-//	system("pause");
-//    return 0;
-//}
-
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
@@ -61,15 +29,10 @@ for(int i=0;i<n+1;i++)
 	(*wierzcholek).dane=liczba;// wprowadzenie danej
 	(*wierzcholek).wsk=temp;// powiązanie nowego elementu z elementem, który wcześniej był wierzchołkiem
 }
-//cout<<" Elementy na stosie: "<<endl;
-//// drukowanie elementów stosu
-temp=wierzcholek;
-//while ( temp!= NULL)
-//{cout<<(*temp).dane<<endl;
-//temp=(*temp).wsk;
-//}  
+temp=wierzcholek; 
 cout << "Podaj x dla ktorego ma zostac wyznaczona wartosc wielomianu: ";
 cin >> x;
+cout << "Elementy na stosie: " << endl;
 wynik = (int)(*temp).dane;
 for (int i = n - 1; i >= 0; i--)
 {
